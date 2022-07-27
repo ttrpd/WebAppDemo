ae50734d2b927e8aff8fdd898a4f56c41baa1140
7429138 <liam.bell@mines.sdsmt.edu>
2022-07-26 20:26:40 -0600

    #2 #1 Implemented storing the submitted user in a local MySQL database

--- server.js
+++ server.js
@@ -2,6 +2,40 @@ var http = require('http');
 var fs = require('fs');
 var url = require('url');
 var formidable = require('formidable');
+var mysql = require('mysql');
+
+var db = mysql.createConnection({
+    host: "localhost",
+    user: "demo_db_user",
+    password: "3mov6@js60ki",
+    insecureAuth: true
+});
+
+// Initialize database
+db.connect(function(err) {
+    if(err) throw err;
+
+    // Ensure the database exists
+    console.log("Connected!");
+    db.query("CREATE DATABASE IF NOT EXISTS demo_db", function(err) {
+        if(err) throw err;
+        console.log("database has been created");
+    });
+
+    // Select the database
+    db.query("USE demo_db", function(err){
+        if(err) throw err;
+    });
+
+    // Ensure that the Users table exists in the database
+    db.query(
+        "CREATE TABLE IF NOT EXISTS Users (Username varchar(255) NOT NULL UNIQUE, Email varchar(255) NOT NULL)",
+        function(err) {
+            if(err) throw err;
+            console.log("user table has been created");
+        }
+    );
+});
 
 // Server code
 http.createServer(function(req, res) {
@@ -12,10 +46,37 @@ http.createServer(function(req, res) {
                 console.error(err.message);
                 return;
             }
-            console.log("Username: "+fields['username']+", Email: "+fields['email']);
+            // Record new user in the database
+            var username = db.escape(fields['username']);// Sanitize input
+            var email = db.escape(fields['email']);// Sanitize input
+            var insert_query = "INSERT INTO Users VALUES ("+username+", "+email+")";
+            console.log("adding new user in persistence");
+            db.query(insert_query, function(err){
+                if(err) {// If the insert fails
+                    // Display an error page to the user
+                    res.writeHead(200, {'content-type': 'text/html'});
+                    fs.readFile('./user_creation_error.html', function(err, data) {
+                        if(err) {
+                            res.writeHead(404, {'Content-Type': 'text/html'});
+                            return res.end("404 Not Found");
+                        } 
+                        res.writeHead(200, {'Content-Type': 'text/html'});
+                        res.write(data);
+                        return res.end();
+                    });
+                } else {// If the insert succeeds
+                    // Display a confirmation page to the user
+                    res.writeHead(200, {'content-type': 'text/html'});
+                    fs.readFile('./confirmation.html', function(err, data) {
+                        if(err) {
+                            return res.end("404 Not Found");
+                        } 
+                        res.write(data);
+                        return res.end();
+                    });
+                }
+            });
         });
-        // Record new user in the database
-        // Send new user a confirmation email
     } else {
         // Serve web page
         var path = '.'+url.parse(req.url).pathname
